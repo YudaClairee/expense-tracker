f2c0c47845e74e6544effcfb0e80982f58b40975
Djibrani Yuda <djibyuda@gmail.com>
2025-08-11 19:35:06 +0700

    Add Clerk Authentication

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { checkUser } from '@/lib/checkUser'
+import React from 'react'
+
+export default function Navbar() {
+
+  const user = checkUser();
+
+  return (
+    <div>Navbar</div>
+  )
+}
